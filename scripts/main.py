7ae93ce898d8d4d747473dc0db872c6ae39d9108
StevenMHernandez <contact@stevenhdesigns.com>
2020-03-26 14:55:56 -0400

    update main.py for attempting 2d-2d

--- scripts/main.py
+++ scripts/main.py
@@ -3,151 +3,119 @@ import math
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+import pandas as pd
 
 # ATTRIBUTE = "Calibrated Distance"
+from scripts.steps.load_data import load_image
+
 ATTRIBUTE = "Confidence map"
 # ATTRIBUTE = "Amplitude"
 
-DETECTOR = "ORB"
-# DETECTOR = "SIFT"
+# DETECTOR = "ORB"
+DETECTOR = "SIFT"
 # DETECTOR = "SURF"
 TOP_K_MATCHES = 300
 MAX_HOMOGRAPHY_ITERATIONS = 250
 
-img1 = cv2.imread('../data/output/RV_Data/Pitch/' + ATTRIBUTE + '/0001/-31.png', 0)  # queryImage
-img2 = cv2.imread('../data/output/RV_Data/Pitch/' + ATTRIBUTE + '/0001/-34.png', 0)  # trainImage
-# img2 = cv2.imread('../data/output/RV_Data/Pitch/' + ATTRIBUTE + '/0001/-37.png', 0)  # trainImage
-# img2 = cv2.imread('../data/output/RV_Data/Pitch/' + ATTRIBUTE + '/0001/-40.png', 0)  # trainImage
-# img2 = cv2.imread('../data/output/RV_Data/Pitch/' + ATTRIBUTE + '/0002/-31.png', 0)  # trainImage
+list_t = []
+list_R = []
 
+for i in range(1,10):
+    print(i)
 
-if DETECTOR == "ORB":
-    detector = cv2.ORB_create()
-elif DETECTOR == "SIFT":
-    detector = cv2.xfeatures2d.SIFT_create()
-elif DETECTOR == "SURF":
-    detector = cv2.xfeatures2d.SURF_create()
-else:
-    print("Detector ({}) unknown".format(DETECTOR))
-    exit(-1)
+    img1, depth_img1 = load_image("../data/RV_Data/Pitch/d1_-40/", "d1_{0:04d}.dat".format(i), ATTRIBUTE)
+    # img2, depth_img2 = load_image("../data/RV_Data/Pitch/d1_-40/", "d1_{0:04d}.dat".format(i), ATTRIBUTE)
+    img2, depth_img2 = load_image("../data/RV_Data/Pitch/d3_-34/", "d3_{0:04d}.dat".format(i), ATTRIBUTE)
 
-kp1, des1 = detector.detectAndCompute(img1, None)
-kp2, des2 = detector.detectAndCompute(img2, None)
+    if DETECTOR == "ORB":
+        detector = cv2.ORB_create()
+    elif DETECTOR == "SIFT":
+        detector = cv2.xfeatures2d.SIFT_create()
+    elif DETECTOR == "SURF":
+        detector = cv2.xfeatures2d.SURF_create()
+    else:
+        print("Detector ({}) unknown".format(DETECTOR))
+        exit(-1)
 
-# create BFMatcher object
-bf = cv2.BFMatcher(cv2.NORM_HAMMING if DETECTOR == "ORB" else cv2.NORM_L1, crossCheck=(DETECTOR == "ORB"))
+    kp1, des1 = detector.detectAndCompute(img1, None)
+    kp2, des2 = detector.detectAndCompute(img2, None)
 
-# Match descriptors.
-matches = bf.match(des1, des2)
-# Sort them in the order of their distance.
-print(matches)
-matches = sorted(matches, key=lambda x: x.distance)
-img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches, None, flags=2)
-plt.imshow(img3)
-plt.show()
+    # create BFMatcher object
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING if DETECTOR == "ORB" else cv2.NORM_L1, crossCheck=False)
 
-
-plt.plot([x.distance for x in matches])
-plt.show()
-
-print(kp1)
-print(len(kp1))
-print(len(kp2))
-print("====")
-
-top_matches = matches[:TOP_K_MATCHES]  # ???
-
-
-points1 = np.array([list(kp2[m.trainIdx].pt) for m in top_matches])
-points2 = np.array([list(kp1[m.queryIdx].pt) for m in top_matches])
-
-print("points2", points2)
-
-cameraMatrix = np.float64([[1,0,0],
-                           [0,1,0],
-                           [0,0,1]])
-
-E, _ = cv2.findEssentialMat(points1, points2, cameraMatrix=cameraMatrix, method=cv2.RANSAC)
-
-_, R, t, _ = cv2.recoverPose(E, points1, points2, cameraMatrix)
-print("R", R)
-print("t", t)
-
-
-# Checks if a matrix is a valid rotation matrix.
-def isRotationMatrix(R):
-    Rt = np.transpose(R)
-    shouldBeIdentity = np.dot(Rt, R)
-    I = np.identity(3, dtype=R.dtype)
-    n = np.linalg.norm(I - shouldBeIdentity)
-    return n < 1e-6
-
-
-# Calculates rotation matrix to euler angles
-# The result is the same as MATLAB except the order
-# of the euler angles ( x and z are swapped ).
-def rotationMatrixToEulerAngles(R):
-    assert (isRotationMatrix(R))
-
-    sy = math.sqrt(R[0, 0] * R[0, 0] + R[1, 0] * R[1, 0])
-
-    singular = sy < 1e-6
-
-    sin_x = math.sqrt(R[2, 0] * R[2, 0] + R[2, 1] * R[2, 1])
-    if not singular:
-        z1    = math.atan2(R[2,0], R[2,1])     # around z1-axis
-        x      = math.atan2(sin_x,  R[2,2])     # around x-axis
-        z2    = math.atan2(R[0,2], -R[1,2])    # around z2-axis
-    else: # gimbal lock
-        z1    = 0                                         # around z1-axis
-        x      = math.atan2(sin_x,  R[2,2])     # around x-axis
-        z2    = 0                                         # around z2-axis
-
-    return [z1, x, z2]
-
-    # if not singular:
-    #     x = math.atan2(R[2, 1], R[2, 2])
-    #     y = math.atan2(-R[2, 0], sy)
-    #     z = math.atan2(R[1, 0], R[0, 0])
-    # else:
-    #     x = math.atan2(-R[1, 2], R[1, 1])
-    #     y = math.atan2(-R[2, 0], sy)
-    #     z = 0
+    # Match descriptors.
+    matches = bf.match(des1, des2)
+    # Sort them in the order of their distance.
+    matches = sorted(matches, key=lambda x: x.distance)
+    img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches, None, flags=2)
+    # plt.imshow(img3)
+    # plt.show()
     #
-    # return np.array([x, y, z])
+    # plt.plot([x.distance for x in matches])
+    # plt.title("Distances for matched descriptors (low to high)")
+    # plt.show()
+
+    # print("Total matches", len(matches))
+    top_matches = matches[:TOP_K_MATCHES]  # ???
+
+    points1 = np.array([list(kp2[m.trainIdx].pt) for m in top_matches])
+    points2 = np.array([list(kp1[m.queryIdx].pt) for m in top_matches])
+
+    # Figure out transformation matrix
+    height, width = img1.shape
+    h, _ = cv2.findHomography(points1, points2, cv2.RANSAC, maxIters=MAX_HOMOGRAPHY_ITERATIONS)
+    im1Reg = cv2.warpPerspective(img2, h, (width, height))
+    h, _ = cv2.findHomography(points2, points1, cv2.RANSAC, maxIters=MAX_HOMOGRAPHY_ITERATIONS)
+    im2Reg = cv2.warpPerspective(img1, h, (width, height))
+    # plt.imshow(img1)
+    # plt.show()
+    # plt.imshow(img2)
+    # plt.show()
+    # plt.imshow(im1Reg)
+    # plt.show()
+    # plt.imshow(im2Reg)
+    # plt.show()
+
+    # Figure out pose change
+    cameraMatrix = np.float64([[1, 0, 0],
+                               [0, 1, 0],
+                               [0, 0, 1]])
+    E, _ = cv2.findEssentialMat(points1, points2, cameraMatrix, cv2.RANSAC, 0.999, 1.0, mask=None)
+    # print(E)
+    newE, R, t, _ = cv2.recoverPose(E, points1, points2, cameraMatrix)
+    # print("R", R)
+    # print("t", t)
+
+    # print(E, newE)
+
+    list_t.append(t)
+    list_R.append(R)
 
 
-print(rotationMatrixToEulerAngles(R))
-
-h, _ = cv2.findHomography(points1, points2, cv2.RANSAC, maxIters=MAX_HOMOGRAPHY_ITERATIONS)
-height, width = img1.shape
-im1Reg = cv2.warpPerspective(img2, h, (width, height))
-
-plt.imshow(img1)
+print(list_t)
+plt.plot(sorted(np.array(list_t)[:,0,0]))
+plt.plot(sorted(np.array(list_t)[:,1,0]))
+plt.plot(sorted(np.array(list_t)[:,2,0]))
+plt.legend(["x", "y", "z"])
+plt.title("predicted transformation")
 plt.show()
 
-plt.imshow(img2)
+print(list_R)
+plt.plot(sorted(np.array(list_R)[:,0,0]))
+plt.plot(sorted(np.array(list_R)[:,0,1]))
+plt.plot(sorted(np.array(list_R)[:,0,2]))
+plt.plot(sorted(np.array(list_R)[:,1,0]))
+plt.plot(sorted(np.array(list_R)[:,1,1]))
+plt.plot(sorted(np.array(list_R)[:,2,2]))
+plt.plot(sorted(np.array(list_R)[:,2,0]))
+plt.plot(sorted(np.array(list_R)[:,1,1]))
+plt.plot(sorted(np.array(list_R)[:,2,2]))
+plt.legend([
+    "0,0", "0,1", "0,2",
+    "1,0", "1,1", "1,2",
+    "2,0", "2,1", "2,2",
+])
+plt.title("predicted R")
 plt.show()
 
-plt.imshow(im1Reg)
-plt.show()
-
-# print("h", mask_h.shape)
-# print("E", mask_E.shape)
-#
-# print("h", h)
-# print("E", E)
-
-
-# E, mask_E = cv2.findEssentialMat(points1, points2, cameraMatrix=cameraMatrix, method=cv2.RANSAC, prob=0.9, threshold=1.0)
-
-# plt.plot(h, 'r-')
-# ax = plt.twinx()
-# plt.plot(E, 'k:')
-# plt.show()
-#
-# plt.plot(mask_h, 'r-')
-# ax = plt.twinx()
-# plt.plot(mask_E, 'k:')
-# plt.show()
-
+# plt.plot(R)
