- determine final outliers after updating final values for R and t - fix global quaternion calculation

scripts/main_build_g2o.py
@@ -14,37 +14,28 @@ if __name__ == "__main__":
 
     to_evaluate = {
         "path": "../data/RV_Data2/d1_{0:04d}.dat",
-        "files_start": 466,
-        "num_files": 468,
-        # "files_start": 1,
-        # "num_files": 450,
-        # "num_files": 584,
+        "files_start": 1,
+        "num_files": 584,
+        "span": 1000,
     }
 
     settings = {
-        # "KNN_MATCHING_RATIO": 0.3,
-        # "KNN_MATCHING_RATIO": 0,
-
         "RANSAC_ITERATIONS": 25,  # number
         "RANSAC_THRESHOLD": 0.01,  # error (m)
-        "RANSAC_MIN_OVERLAP_DISTANCE": 0.25,  # cm
+        "RANSAC_MIN_OVERLAP_DISTANCE": 0.125,  # cm
         "MAX_PAIR_MOVEMENT_DISTANCE": 25,  # cm
         "MAX_ALLOWED_POSE_CHANGE_XYZ": 75,  # cm
         "MIN_NUMBER_OF_INLIERS": 8,  # number
 
-        # "MEDIAN_BLUR": True,
         "MEDIAN_BLUR": False,
-        # "GAUSSIAN_BLUR": True,
         "GAUSSIAN_BLUR": False,
     }
 
     f = open("../output/output.g2o", "w")
 
     base_data_path = to_evaluate["path"]
-    vertex_output = "VERTEX_SE3:QUAT 0 0 0 0 0 0 0 1 \n"
+    vertex_output = "VERTEX_SE3:QUAT 0 0 0 0 1 0 0 0 \n"
     edge_output = ""
-    new_t = [0, 0, 0]
-    new_r_q = [0, 0, 0, 1]
 
     num_inliers_matrix = np.zeros(
         [to_evaluate["num_files"] - to_evaluate["files_start"], to_evaluate["num_files"] - to_evaluate["files_start"]])
@@ -58,7 +49,7 @@ if __name__ == "__main__":
         previous_global_xyz_sum = global_xyz_sum
         previous_global_quaternions_sum = global_quaternions_sum
 
-        for image_j in range(image_i + 1, min(image_i + 2, to_evaluate["num_files"])):
+        for image_j in range(image_i + 1, min(image_i + 1 + to_evaluate["span"], to_evaluate["num_files"])):
         # for image_j in range(image_i + 1, to_evaluate["num_files"]):
             if image_i != image_j:
                 print()
@@ -84,7 +75,7 @@ if __name__ == "__main__":
                             final_r_q, _, final_t, inliers, informationMatrix = predict_pose_change(
                                 base_data_path.format(image_i),
                                 base_data_path.format(image_j), _settings,
-                                real_change=None, CALCULATE_ERROR=False, print_image=True)
+                                real_change=None, CALCULATE_ERROR=False, print_image=False)
 
                             print("inlier:", len([x for x in inliers if x]))
                             break
@@ -106,8 +97,7 @@ if __name__ == "__main__":
                         continue
 
                 num_inliers = len([x for x in inliers if x])
-                num_inliers_matrix[
-                    image_i - to_evaluate["files_start"], image_j - to_evaluate["files_start"]] = num_inliers
+                num_inliers_matrix[image_i - to_evaluate["files_start"], image_j - to_evaluate["files_start"]] = num_inliers
 
                 #
                 # Compute: local-change and global-change: (sum of local changes).
@@ -116,11 +106,10 @@ if __name__ == "__main__":
                     if image_i == 1:
                         global_xyz_sum = previous_global_xyz_sum + np.array(final_t)
                     else:
-                        global_xyz_sum = previous_global_xyz_sum + (previous_global_quaternions_sum).rotate(
-                            np.array(final_t))
-                    global_quaternions_sum *= Quaternion(final_r_q[3], final_r_q[0], final_r_q[1], final_r_q[2])
-                    output_string = " ".join([str(x) for x in global_xyz_sum]) + " " + " ".join(
-                        [str(x) for x in final_r_q])
+                        global_xyz_sum = previous_global_xyz_sum + (previous_global_quaternions_sum).rotate(np.array(final_t))
+                    global_quaternions_sum *= Quaternion(final_r_q)
+                    # global_quaternions_sum *= Quaternion(final_r_q[3], final_r_q[0], final_r_q[1], final_r_q[2])
+                    output_string = " ".join([str(x) for x in global_xyz_sum]) + " " + " ".join([str(x) for x in global_quaternions_sum])
                     vertex_output += "VERTEX_SE3:QUAT " + str(image_i) + " " + output_string + " \n"
 
                 output_string = " ".join([str(x) for x in final_t]) + " " + " ".join([str(x) for x in final_r_q])

scripts/steps/ransac.py
@@ -7,7 +7,7 @@ from scripts.steps.estimate_R_and_t import estimate_R_and_t
 
 
 def ransac(p, p_prime, settings):
-    th = settings['RANSAC_THRESHOLD']
+    threshold = settings['RANSAC_THRESHOLD']
 
     indices = list(range(p.shape[1]))
 
@@ -17,35 +17,59 @@ def ransac(p, p_prime, settings):
     predictions_ignored = 0
 
     for k in range(settings["RANSAC_ITERATIONS"]):
+        #
+        # Select 4 pairs randomly
+        #
         shuffle(indices)
         p_m = p[:,indices[:4]]
         p_prime_m = p_prime[:,indices[:4]]
 
+        #
+        # Ignore selected pairs in {p_m and p_prime_m} which are too close or overlapping.
+        # Such overlapping pairs often result in bad predictions
+        #
         for i in range(4):
             for j in range(4):
                 if norm(p_m[:, i] - p_prime_m[:, j]) < settings['RANSAC_MIN_OVERLAP_DISTANCE']:
-                    # If any of the selected pairs in {p_m and p_prime_m} are too close (i.e. overlapping)
-                    # ignore this prediction
                     predictions_ignored += 1
                     continue
 
 
+        #
+        # Make estimation based on 4 randomly selected pairs
+        #
         R,t = estimate_R_and_t(p_m, p_prime_m)
 
+        #
+        # Using predicted R and t to calculate maximum support (max_S_k)
+        #
         p_projected = R.dot(p) + t
         error = norm(p_prime - p_projected, axis=0)
-
-        if (error < th).sum() > max_S_k:
-            max_S_k = (error < th).sum()
-            inliers = deepcopy(list(error < th))
+        num_supporting_this_hypothesis = (error < threshold).sum()
+        if num_supporting_this_hypothesis > max_S_k:
+            max_S_k = num_supporting_this_hypothesis
+            inliers = deepcopy(list(error < threshold))
 
     if predictions_ignored > 0:
         print("predictions_ignored:", predictions_ignored)
 
-    num_inliers = len([x for x in inliers if x])
+    #
+    # Make final prediction using all inliers
+    #
+    R,t = estimate_R_and_t(p[:,inliers], p_prime[:,inliers])
 
+    #
+    # Determine final inliers
+    #
+    p_projected = R.dot(p) + t
+    error = norm(p_prime - p_projected, axis=0)
+    inliers = deepcopy(list(error < threshold))
+
+    #
+    # Calculate information matrix
     # Based on: (https://github.com/rising-turtle/visual_odometry/blob/master/src/VRO/camera_node.cpp#L78)
+    #
+    num_inliers = len([x for x in inliers if x])
     informationMatrix = np.identity(6) * (num_inliers / (sum(error[inliers])**2))
 
-    R,t = estimate_R_and_t(p[:,inliers], p_prime[:,inliers])
     return R,t,inliers,informationMatrix